3f64a85e87a1daaee98936cb0aa5ef1f2acdf367
Fernando Vaz <contact@fernandomvaz.com>
2021-01-04 13:39:23 -0300

    Adição de evento de Keypress em CPP

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "include/SerialPort.h"
+#include "winuser.h"
 #include <cstdio>
 #include <string.h>
 #include <fstream>
 #include <regex>
+#include <winable.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 using namespace std;
 
@@ -15,10 +23,10 @@ char incomingData[MAX_DATA_LENGTH_ARDUINO];
 //Arduino SerialPort object
 SerialPort *arduino;
 
-
 int main()
 {
     //ofstream numbers;
+    INPUT ip;
 
     arduino = new SerialPort(portName);
     std::cout << portName << std::endl;
@@ -28,10 +36,28 @@ int main()
     while(arduino->isConnected())
     {
         hasRead = arduino ->readSerialPort(incomingData, MAX_DATA_LENGTH_ARDUINO);
-        if(hasRead)
-        {
+        if(hasRead) {
+            // Set up a generic keyboard event.
+            ip.type = INPUT_KEYBOARD;
+
+            // Press the "A" key
+            ip.ki.wVk = 0x41;
+            ip.ki.dwFlags = 0;
+            SendInput(1, &ip, sizeof(INPUT));
+
             std::cout << incomingData << std::endl;
         }
+        Sleep(50);
     }
+
+    // This structure will be used to create the keyboard
+    // input event.
+    //
+
+
+
+
+    // Exit normally
+    return 0;
 }
 
